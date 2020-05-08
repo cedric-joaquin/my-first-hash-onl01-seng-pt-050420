a569071f93fbfa5df397bc55e41622f12a04e09a
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-08 16:36:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,6 +32,8 @@ def adding
                   "oil paintings" => 3
                  }
 
-  shipping_manifest["gun powder"] =
+  shipping_manifest["gun powder"] = 4
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest
 
 end
